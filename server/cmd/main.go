430e89a49671106fe4000dea1aa40535988fba84
Pedro Henrique <pedrovasalmeida@gmail.com>
2023-10-07 14:45:34 -0300

    feat: initialize router

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,11 +3,20 @@ package main
 import (
 	"log"
 	"server/db"
+	"server/internal/user"
+	"server/router"
 )
 
 func main() {
-	_, err := db.NewDatabase()
+	dbConn, err := db.NewDatabase()
 	if err != nil {
 		log.Fatalf("Could not initialize database connection: %s", err)
 	}
+
+	userRepository := user.NewRepository(dbConn.GetDB())
+	userService := user.NewService(userRepository)
+	userHandler := user.NewHandler(userService)
+
+	router.InitRouter(userHandler)
+	router.Start("localhost:8080")
 }
