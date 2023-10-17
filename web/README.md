961408eaa836612fa971a733b2da4041e22647d3
Pedro Henrique <pedrovasalmeida@gmail.com>
2023-10-17 07:18:14 -0300

    feat: setup web project with next.js and tailwind

---
+++ web/README.md
@@ -0,0 +1,11 @@
+### Requisitos
+
+<!-- Hub -->
+[ ] Usuário deve conseguir ver a lista de salas disponíveis
+[ ] Usuário deve conseguir criar uma nova sala
+[ ] Usuário deve conseguir acessar uma das salas disponíveis
+
+<!-- Room -->
+[ ] Usuário deve conseguir ver a lista de mensagens enviadas
+[ ] Usuário deve conseguir enviar uma nova mensagem
+[ ] Usuário deve conseguir sair da sala
---
+++ web/src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'GoChat',
+  description: 'My realtime chat created with Next.js and Golang.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
